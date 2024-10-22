0d296089de22696d52720168c21ebb3ba8ae73e6
WhyAsh5114 <whyash5114@gmail.com>
2024-10-22 17:06:28 +0530

    feat: added enum support

--- packages/generator/src/generator.ts
+++ packages/generator/src/generator.ts
@@ -1,13 +1,12 @@
 import { generatorHandler, GeneratorOptions } from "@prisma/generator-helper";
-import { logger } from "@prisma/internals";
 import { DBSchema } from "idb";
-import { GENERATOR_NAME } from "./constants";
+import path from "path";
 import {
   convertToInterface,
+  generateEnumObject,
   prismaToIDBTypeMap,
   writeFileSafely,
 } from "./utils";
-import path from "path";
 
 const { version } = require("../package.json");
 
@@ -16,24 +15,34 @@ generatorHandler({
     return {
       version,
       defaultOutput: "../generated",
-      prettyName: GENERATOR_NAME,
     };
   },
 
   onGenerate: async (options: GeneratorOptions) => {
     const schema: DBSchema = {};
+    let enumText = "";
+
+    const enumNames = options.dmmf.datamodel.enums.map(({ name }) => name);
+    const prismaToIDBTypeMapWithEnums = new Map(prismaToIDBTypeMap);
+    enumNames.forEach((enumName) =>
+      prismaToIDBTypeMapWithEnums.set(enumName, enumName),
+    );
+
+    options.dmmf.datamodel.enums.forEach(async (enumField) => {
+      enumText += generateEnumObject(enumField);
+    });
 
     options.dmmf.datamodel.models.forEach((model) => {
       const key = model.fields.find(({ isId }) => isId);
       if (!key)
         throw new Error(
-          `Error during PrismaIDB: No id field found for model: ${model.name}`
+          `Error during PrismaIDB: No id field found for model: ${model.name}`,
         );
 
       const mappedKeyType = prismaToIDBTypeMap.get(key.type);
       if (!mappedKeyType) {
         throw new Error(
-          `Error during PrismaIDB: Prisma type ${key.type} is not yet supported`
+          `Error during PrismaIDB: Key of type ${key.type} is not yet supported`,
         );
       }
 
@@ -41,10 +50,10 @@ generatorHandler({
       model.fields.forEach((field) => {
         if (field.kind === "object") return;
 
-        const mappedType = prismaToIDBTypeMap.get(field.type);
+        const mappedType = prismaToIDBTypeMapWithEnums.get(field.type);
         if (!mappedType) {
           throw new Error(
-            `Error during PrismaIDB: Prisma type ${field.type} is not yet supported`
+            `Error during PrismaIDB: Prisma type ${field.type} is not yet supported`,
           );
         }
 
@@ -54,10 +63,14 @@ generatorHandler({
       schema[model.name] = { key: mappedKeyType, value };
     });
 
-    const tsTypes = convertToInterface(schema);
+    const tsTypes = convertToInterface(
+      schema,
+      enumText,
+      prismaToIDBTypeMapWithEnums,
+    );
     const writeLocation = path.join(
       options.generator.output?.value!,
-      `prisma-idb-types.ts`
+      `prisma-idb-types.ts`,
     );
 
     await writeFileSafely(writeLocation, tsTypes);
--- packages/generator/src/utils.ts
+++ packages/generator/src/utils.ts
@@ -2,6 +2,7 @@ import { DBSchema } from "idb";
 import fs from "fs";
 import path from "path";
 import prettier from "prettier";
+import { DMMF } from "@prisma/generator-helper";
 
 export const formatFile = (content: string): Promise<string> => {
   return new Promise((res, rej) =>
@@ -18,7 +19,7 @@ export const formatFile = (content: string): Promise<string> => {
       } catch (error) {
         rej(error);
       }
-    })
+    }),
   );
 };
 
@@ -30,8 +31,14 @@ export const prismaToIDBTypeMap = new Map([
   ["DateTime", "Date"],
 ]);
 
-export function convertToInterface(schema: DBSchema): string {
+export function convertToInterface(
+  schema: DBSchema,
+  enumText: string,
+  typeMapWithEnums: Map<string, string>,
+): string {
   let result = `import type { DBSchema } from "idb";\n\n`;
+
+  result += `${enumText};\n\n`;
   result += `export interface PrismaIDBSchema extends DBSchema {\n`;
 
   for (const modelName in schema) {
@@ -41,7 +48,15 @@ export function convertToInterface(schema: DBSchema): string {
 
     result += `    value: {\n`;
     for (const field in model.value) {
-      result += `      ${field}: ${model.value[field]};\n`;
+      let fieldType = model.value[field];
+      console.log(prismaToIDBTypeMap, typeMapWithEnums);
+      if (
+        !prismaToIDBTypeMap.get(fieldType) &&
+        typeMapWithEnums.get(fieldType)
+      ) {
+        fieldType = `typeof ${fieldType}[keyof typeof ${fieldType}]`;
+      }
+      result += `      ${field}: ${fieldType};\n`;
     }
     result += `    };\n`;
   }
@@ -49,6 +64,11 @@ export function convertToInterface(schema: DBSchema): string {
 
   result += `}\n`;
   return result;
+}
+
+export function generateEnumObject({ name, values }: DMMF.DatamodelEnum) {
+  const enumValues = values.map(({ name }) => `${name}:"${name}"`).join(",\n");
+  return `export const ${name} = { \n${enumValues}\n } as const;\n\n`;
 }
 
 export const writeFileSafely = async (writeLocation: string, content: any) => {
