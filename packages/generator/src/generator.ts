168512d1761adc42c41aedc78a471b84adbd271b
WhyAsh5114 <whyash5114@gmail.com>
2024-10-27 15:33:55 +0530

    feat: use ts-morph to generate files

--- packages/generator/src/generator.ts
+++ packages/generator/src/generator.ts
@@ -1,13 +1,7 @@
 import { generatorHandler, GeneratorOptions } from "@prisma/generator-helper";
-import { DBSchema } from "idb";
 import path from "path";
-import {
-  convertToInterface,
-  generateEnumObject,
-  generateIDBKey,
-  prismaToIDBTypeMap,
-  writeFileSafely,
-} from "./utils";
+import { Project, Scope, StructureKind, VariableDeclarationKind } from "ts-morph";
+import { generateIDBKey, toCamelCase, writeFileSafely } from "./utils";
 
 const { version } = require("../package.json");
 
@@ -20,48 +14,154 @@ generatorHandler({
   },
 
   onGenerate: async (options: GeneratorOptions) => {
-    const schema: DBSchema = {};
-    let enumText = "";
+    // TODO: handle enums
+    const { models, enums } = options.dmmf.datamodel;
 
-    const enumMap = new Map(
-      options.dmmf.datamodel.enums.map(({ name }) => [name, name]),
-    );
+    const project = new Project();
+    const file = project.createSourceFile(path.join("prisma-idb-client.ts"), "", { overwrite: true });
 
-    options.dmmf.datamodel.enums.forEach(async (enumField) => {
-      enumText += generateEnumObject(enumField);
+    file.addImportDeclaration({
+      moduleSpecifier: "idb",
+      namedImports: ["openDB"],
+    });
+    file.addImportDeclaration({
+      moduleSpecifier: "idb",
+      namedImports: ["IDBPDatabase"],
+      isTypeOnly: true,
+    });
+    file.addImportDeclaration({
+      moduleSpecifier: "@prisma/client",
+      namedImports: ["Prisma"],
+      isTypeOnly: true,
     });
 
-    options.dmmf.datamodel.models.forEach((model) => {
-      const value: Record<string, string> = {};
-      model.fields.forEach((field) => {
-        if (field.kind === "object") return;
+    // TODO: update version numbers if schema changes
+    file.addVariableStatement({
+      declarationKind: VariableDeclarationKind.Const,
+      declarations: [{ name: "IDB_VERSION", type: "number", initializer: "1" }],
+    });
 
-        const enumType = enumMap.get(field.type);
-        if (enumType) {
-          value[field.name] = `typeof ${enumType}[keyof typeof ${enumType}]`;
-          return;
-        }
+    file.addClass({
+      name: "PrismaIDBClient",
+      isExported: true,
+      ctors: [{ scope: Scope.Private }],
+      properties: [
+        { name: "instance", isStatic: true, type: "PrismaIDBClient", scope: Scope.Private },
+        { name: "db", type: "IDBPDatabase", hasExclamationToken: true },
+        ...models.map((model) => ({
+          name: toCamelCase(model.name),
+          type: `IDB${model.name}`,
+          hasExclamationToken: true,
+        })),
+      ],
+      methods: [
+        {
+          name: "getInstance",
+          isStatic: true,
+          isAsync: true,
+          returnType: "Promise<PrismaIDBClient>",
+          statements: (writer) => {
+            writer
+              .writeLine("if (!PrismaIDBClient.instance) {")
+              .indent(() => {
+                writer
+                  .writeLine("PrismaIDBClient.instance = new PrismaIDBClient();")
+                  .writeLine("await PrismaIDBClient.instance.createDatabase();");
+              })
+              .writeLine("}")
+              .writeLine("return PrismaIDBClient.instance;");
+          },
+        },
+        {
+          name: "createDatabase",
+          scope: Scope.Protected,
+          isAsync: true,
+          statements: (writer) => {
+            writer
+              .writeLine("this.db = await openDB('prisma-idb', IDB_VERSION, {")
+              .indent(() => {
+                writer
+                  .writeLine("upgrade(db) {")
+                  .indent(() => {
+                    models.forEach((model) => {
+                      writer.writeLine(
+                        `db.createObjectStore('${toCamelCase(model.name)}', { keyPath: ${generateIDBKey(model)} });`,
+                      );
+                    });
+                  })
+                  .writeLine("}");
+              })
+              .writeLine("});");
 
-        const mappedType = prismaToIDBTypeMap.get(field.type);
-        if (mappedType) {
-          value[field.name] = mappedType;
-          return;
-        }
+            models.forEach((model) => {
+              writer.writeLine(`this.${toCamelCase(model.name)} = new IDB${model.name}(this.db);`);
+            });
+          },
+        },
+      ],
+    });
 
-        throw new Error(
-          `Error during PrismaIDB: Prisma type ${field.type} is not yet supported`,
-        );
+    file.addClass({
+      name: "BaseIDBModelClass",
+      properties: [{ name: "db", type: "IDBPDatabase" }],
+      ctors: [
+        {
+          parameters: [{ name: "db", type: "IDBPDatabase" }],
+          statements: (writer) => writer.writeLine("this.db = db"),
+        },
+      ],
+    });
+
+    models.forEach((model) => {
+      file.addClass({
+        name: `IDB${model.name}`,
+        extends: "BaseIDBModelClass",
+        methods: [
+          {
+            name: "findFirst",
+            isAsync: true,
+            parameters: [{ name: "query", type: `Prisma.${model.name}FindFirstArgs` }],
+            statements: (writer) => {
+              // TODO: full prisma query mapping, first perform get or getAll and filter based on query
+              writer.writeLine(`const records = await this.db.getAll("${toCamelCase(model.name)}");`);
+              // TODO: apply filters according to query
+              writer.writeLine("return records;");
+            },
+          },
+          {
+            name: "findMany",
+            isAsync: true,
+            parameters: [{ name: "query", type: `Prisma.${model.name}FindManyArgs` }],
+            statements: (writer) => {
+              // TODO: full prisma query mapping
+              writer.writeLine(`return await this.db.getAll("${toCamelCase(model.name)}");`);
+            },
+          },
+          {
+            name: "findUnique",
+            isAsync: true,
+            parameters: [{ name: "query", type: `Prisma.${model.name}FindUniqueArgs` }],
+            statements: (writer) => {
+              // TODO: full prisma query mapping (@unique/@id modifier) (@@id composite keys)
+              writer.writeLine(
+                `return (await this.db.get("${toCamelCase(model.name)}", [query.where.id as number])) ?? null;`,
+              );
+            },
+          },
+          {
+            name: "create",
+            isAsync: true,
+            parameters: [{ name: "query", type: `Prisma.${model.name}CreateArgs` }],
+            statements: (writer) => {
+              // TODO: full prisma query mapping (@unique/@id modifier) (@@id composite keys)
+              writer.writeLine(`await this.db.add("${model.name}", query);`);
+            },
+          },
+        ],
       });
-
-      schema[model.name] = { key: generateIDBKey(model), value };
     });
 
-    const fileOutput = convertToInterface(schema, enumText);
-    const writeLocation = path.join(
-      options.generator.output?.value!,
-      `prisma-idb-types.ts`,
-    );
-
-    await writeFileSafely(writeLocation, fileOutput);
+    const writeLocation = path.join(options.generator.output?.value!, file.getBaseName());
+    await writeFileSafely(writeLocation, file.getText());
   },
 });
---
+++ packages/usage/types/prisma-idb-client.ts
@@ -0,0 +1,123 @@
+import { openDB } from "idb";
+import type { IDBPDatabase } from "idb";
+import type { Prisma } from "@prisma/client";
+
+const IDB_VERSION: number = 1;
+
+export class PrismaIDBClient {
+  private static instance: PrismaIDBClient;
+  db!: IDBPDatabase;
+  user!: IDBUser;
+  account!: IDBAccount;
+  session!: IDBSession;
+  verificationToken!: IDBVerificationToken;
+
+  private constructor() {}
+
+  static async getInstance(): Promise<PrismaIDBClient> {
+    if (!PrismaIDBClient.instance) {
+      PrismaIDBClient.instance = new PrismaIDBClient();
+      await PrismaIDBClient.instance.createDatabase();
+    }
+    return PrismaIDBClient.instance;
+  }
+
+  protected async createDatabase() {
+    this.db = await openDB("prisma-idb", IDB_VERSION, {
+      upgrade(db) {
+        db.createObjectStore("user", { keyPath: ["id"] });
+        db.createObjectStore("account", { keyPath: ["provider", "providerAccountId"] });
+        db.createObjectStore("session", { keyPath: ["sessionToken"] });
+        db.createObjectStore("verificationToken", { keyPath: ["identifier", "token"] });
+      },
+    });
+    this.user = new IDBUser(this.db);
+    this.account = new IDBAccount(this.db);
+    this.session = new IDBSession(this.db);
+    this.verificationToken = new IDBVerificationToken(this.db);
+  }
+}
+
+class BaseIDBModelClass {
+  db: IDBPDatabase;
+
+  constructor(db: IDBPDatabase) {
+    this.db = db;
+  }
+}
+
+class IDBUser extends BaseIDBModelClass {
+  async findFirst(query: Prisma.UserFindFirstArgs) {
+    const records = await this.db.getAll("user");
+    return records;
+  }
+
+  async findMany(query: Prisma.UserFindManyArgs) {
+    return await this.db.getAll("user");
+  }
+
+  async findUnique(query: Prisma.UserFindUniqueArgs) {
+    return (await this.db.get("user", [query.where.id as number])) ?? null;
+  }
+
+  async create(query: Prisma.UserCreateArgs) {
+    await this.db.add("User", query);
+  }
+}
+
+class IDBAccount extends BaseIDBModelClass {
+  async findFirst(query: Prisma.AccountFindFirstArgs) {
+    const records = await this.db.getAll("account");
+    return records;
+  }
+
+  async findMany(query: Prisma.AccountFindManyArgs) {
+    return await this.db.getAll("account");
+  }
+
+  async findUnique(query: Prisma.AccountFindUniqueArgs) {
+    return (await this.db.get("account", [query.where.id as number])) ?? null;
+  }
+
+  async create(query: Prisma.AccountCreateArgs) {
+    await this.db.add("Account", query);
+  }
+}
+
+class IDBSession extends BaseIDBModelClass {
+  async findFirst(query: Prisma.SessionFindFirstArgs) {
+    const records = await this.db.getAll("session");
+    return records;
+  }
+
+  async findMany(query: Prisma.SessionFindManyArgs) {
+    return await this.db.getAll("session");
+  }
+
+  async findUnique(query: Prisma.SessionFindUniqueArgs) {
+    return (await this.db.get("session", [query.where.id as number])) ?? null;
+  }
+
+  async create(query: Prisma.SessionCreateArgs) {
+    await this.db.add("Session", query);
+  }
+}
+
+class IDBVerificationToken extends BaseIDBModelClass {
+  async findFirst(query: Prisma.VerificationTokenFindFirstArgs) {
+    const records = await this.db.getAll("verificationToken");
+    return records;
+  }
+
+  async findMany(query: Prisma.VerificationTokenFindManyArgs) {
+    return await this.db.getAll("verificationToken");
+  }
+
+  async findUnique(query: Prisma.VerificationTokenFindUniqueArgs) {
+    return (await this.db.get("verificationToken", [query.where?.id as number])) ?? null;
+  }
+
+  async create(query: Prisma.VerificationTokenCreateArgs) {
+    await this.db.add("VerificationToken", query);
+  }
+}
