5381f4662923565a1d3ca94425f0ea073fe2d489
WhyAsh5114 <whyash5114@gmail.com>
2024-10-28 12:23:02 +0530

    fix: reduce config duplication

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,7 @@ import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
 export default [
-	{ ignores: ['packages/generator/dist/*'] },
+	{ ignores: ['packages/generator/dist/*', 'packages/usage/.svelte-kit/*'] },
 	{ files: ['**/*.{js,mjs,cjs,ts}'] },
 	{ languageOptions: { globals: globals.browser } },
 	pluginJs.configs.recommended,
