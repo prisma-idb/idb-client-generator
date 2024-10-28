bbe584ddb36752f2f760461f80643e8b60904d7d
sundaram123krishnan <krishnsundaram@gmail.com>
2024-10-28 20:44:34 +0530

    feat: todo ui

--- packages/usage/src/routes/+page.svelte
+++ packages/usage/src/routes/+page.svelte
@@ -1,21 +1,55 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-	import { PrismaIDBClient } from '$lib/prisma-idb/prisma-idb-client';
+  import { onMount } from "svelte";
+  import { v4 as uuidv4 } from "uuid";
+  import { PrismaIDBClient } from "$lib/prisma-idb/prisma-idb-client";
+  import { Input } from "$lib/components/ui/input/index.js";
+  import { Button } from "$lib/components/ui/button";
 
-	let client: PrismaIDBClient;
+  let client: PrismaIDBClient;
+  let isCompleted = $state(false);
 
-	onMount(async () => {
-		// Always instantiate on client-side (need IndexedDB)
-		client = await PrismaIDBClient.getInstance();
-	});
+  const formData = {
+    id: "",
+    task: "",
+	isCompleted,
+  };
+
+  let task = "";
+
+  let id: String;
+
+
+  function handleChange(event: Event) {
+    const target = event.target as HTMLSelectElement;
+    task = target.value;
+  }
+
+
+  function AddTask() {
+    let id = uuidv4();
+    formData.task = task;
+    formData.id = id;
+	formData.isCompleted = isCompleted;
+    console.log(formData);
+    client.todo.create({ data: formData });
+	console.log(client);
+  }
+
+  onMount(async () => {
+    // Always instantiate on client-side (need IndexedDB)
+    client = await PrismaIDBClient.getInstance();
+  });
 </script>
 
-<main class="prose max-w-full p-4">
-	{#if client}
-		<h1>Prisma-IDB usage page</h1>
-		<p>Database version: <span>{client.db.version}</span></p>
-	{:else}
-		<h1>Prisma-IDB-client couldn't load</h1>
-		<p>Check console for more information</p>
-	{/if}
+<main class="prose max-w-full">
+  <div class="space-y-2">
+    <Input
+      type="text"
+      placeholder="Enter task"
+      class="max-w-xs"
+      on:input={handleChange}
+    />
+    <button onclick={AddTask}>Add Task</button>
+	<input type="checkbox" bind:checked={isCompleted}/>
+  </div>
 </main>
