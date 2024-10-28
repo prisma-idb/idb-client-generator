55600c019b17c0275fc0b8bfdc8c91bf4eb256e3
WhyAsh5114 <whyash5114@gmail.com>
2024-10-28 11:51:22 +0530

    feat: setup basic page displaying idb version

--- packages/usage/src/routes/+page.svelte
+++ packages/usage/src/routes/+page.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { PrismaIDBClient } from '$lib/prisma-idb/prisma-idb-client';
+
+	let client: PrismaIDBClient;
+
+	onMount(async () => {
+		// Always instantiate on client-side (need IndexedDB)
+		client = await PrismaIDBClient.getInstance();
+	});
+</script>
+
+<main class="prose max-w-full p-4">
+	{#if client}
+		<h1>Prisma-IDB usage page</h1>
+		<p>Database version: <span>{client.db.version}</span></p>
+	{:else}
+		<h1>Prisma-IDB-client couldn't load</h1>
+		<h2>Check console for more information</h2>
+	{/if}
+</main>
