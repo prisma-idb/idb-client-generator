3e116a782da4f099b4c12159dca589c7e88f5b80
sundaram123krishnan <krishnsundaram@gmail.com>
2024-10-31 19:40:28 +0530

    chore: fix page route

--- packages/usage/tests/test-click.spec.ts
+++ packages/usage/tests/test-click.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from "@playwright/test";
 
 test("test", async ({ page }) => {
-  await page.goto("http://localhost:5173/");
+  await page.goto("/");
   await page.getByRole("button", { name: "Add Task" }).click();
 });
