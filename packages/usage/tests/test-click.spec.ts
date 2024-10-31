3dcebd06e4dc36072c0e8b7c7338fc46e6cac987
sundaram123krishnan <krishnsundaram@gmail.com>
2024-10-31 19:39:11 +0530

    feat: add click test

---
+++ packages/usage/tests/test-click.spec.ts
@@ -0,0 +1,7 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+
+  await page.goto('http://localhost:5173/');
+  await page.getByRole('button', { name: 'Add Task' }).click();
+});
