b14e89ed0abd6a122428e356b217e35004610610
sundaram123krishnan <krishnsundaram@gmail.com>
2024-10-31 22:51:29 +0530

    chore: refactor test

--- packages/usage/tests/test-click.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from "@playwright/test";
-
-test("test", async ({ page }) => {
-  await page.goto("/");
-  await page.getByRole("button", { name: "Add Task" }).click();
-  await page.getByRole("button", { name: "Add Task" }).dblclick();
-  await expect(page.getByRole("button", { name: "Add Task" })).toBeVisible();
-  await expect(page.locator("body")).toContainText("Add Task");
-});
--- packages/usage/tests/test-heading.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from "@playwright/test";
-
-test("test", async ({ page }) => {
-  await page.goto("/");
-  await page.getByRole("heading", { name: "Prisma-IDB usage page" }).dblclick();
-  await page.getByRole("heading", { name: "Prisma-IDB usage page" }).click();
-  await expect(page.getByRole("heading")).toContainText("Prisma-IDB usage page");
-  await expect(page.getByRole("heading", { name: "Prisma-IDB usage page" })).toBeVisible();
-});
