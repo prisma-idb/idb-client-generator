4ac563683118e332d3fc2e8bccd3d385ecad2814
sundaram123krishnan <krishnsundaram@gmail.com>
2024-10-31 22:16:22 +0530

    chore: add assertions

--- packages/usage/tests/test-heading.spec.ts
+++ packages/usage/tests/test-heading.spec.ts
@@ -1,6 +1,9 @@
-import { test } from "@playwright/test";
+import { test, expect } from '@playwright/test';
 
-test("test", async ({ page }) => {
-  await page.goto("/");
-  await page.getByRole("heading", { name: "Prisma-IDB usage page" }).dblclick();
+test('test', async ({ page }) => {
+  await page.goto('/');
+  await page.getByRole('heading', { name: 'Prisma-IDB usage page' }).dblclick();
+  await page.getByRole('heading', { name: 'Prisma-IDB usage page' }).click();
+  await expect(page.getByRole('heading')).toContainText('Prisma-IDB usage page');
+  await expect(page.getByRole('heading', { name: 'Prisma-IDB usage page' })).toBeVisible();
 });
--- packages/usage/tests/test-placeholder.spec.ts
+++ packages/usage/tests/test-placeholder.spec.ts
@@ -1,7 +1,10 @@
-import { test } from "@playwright/test";
+import { test, expect } from '@playwright/test';
 
-test("test", async ({ page }) => {
-  await page.goto("/");
-  await page.getByPlaceholder("Enter Task").click();
-  await page.getByPlaceholder("Enter Task").fill("finish assignments");
+test('test', async ({ page }) => {
+  await page.goto('/');
+  await page.getByPlaceholder('Enter Task').click();
+  await page.getByPlaceholder('Enter Task').fill('finish assignments');
+  await page.getByPlaceholder('Enter Task').press('Enter');
+  await expect(page.getByPlaceholder('Enter Task')).toHaveValue('finish assignments');
+  await expect(page.getByPlaceholder('Enter Task')).toBeVisible();
 });
