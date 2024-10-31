d06cefb13cc465f5bf18860bb22646c7622ba6e1
sundaram123krishnan <krishnsundaram@gmail.com>
2024-10-31 19:10:15 +0530

    chore: fix page route

--- packages/usage/tests/test-placeholder.spec.ts
+++ packages/usage/tests/test-placeholder.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
-  await page.goto('http://localhost:5173/');
+  await page.goto('/');
   await page.getByPlaceholder('Enter Task').click();
   await page.getByPlaceholder('Enter Task').fill('finish assignments');
 });
