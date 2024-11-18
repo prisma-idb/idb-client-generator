25787e6ef3ff659acd21fb57722e4d1f443160b3
WhyAsh5114 <whyash5114@gmail.com>
2024-11-18 11:15:22 +0530

    test: added test for nested create with meta on current field

--- packages/usage/tests/nestedQueries/create.spec.ts
+++ packages/usage/tests/nestedQueries/create.spec.ts
@@ -68,8 +68,28 @@ test("create_WithOneToOneRelationMetaOnNested_SuccessfullyCreatesBothEntities", 
   expect(JSON.parse(idbClientOutput2)).toEqual(result2);
 });
 
-test("create_WithOneToOneRelationMetaOnParent_SuccessfullyCreatesBothEntities", async () => {
-  // TODO
+test("create_WithOneToOneRelationMetaOnParent_SuccessfullyCreatesBothEntities", async ({ page }) => {
+  const query: Prisma.ProfileCreateArgs = {
+    data: { bio: "Alice's bio", user: { create: { name: "Alice" } } },
+  };
+  const result = await prisma.profile.create(query);
+
+  await page.getByTestId("query-input").fill(`profile.create(${JSON.stringify(query)})`);
+  await page.getByRole("button", { name: "Run query" }).click();
+  await expect(page.getByRole("status")).toContainText("Query executed successfully");
+
+  const idbClientOutput = (await page.getByRole("code").textContent()) ?? "";
+  expect(JSON.parse(idbClientOutput)).toEqual(result);
+
+  const query2: Prisma.UserFindUniqueArgs = { where: { id: 1 } };
+  const result2 = await prisma.user.findUnique(query2);
+
+  await page.getByTestId("query-input").fill(`user.findUnique(${JSON.stringify(query2)})`);
+  await page.getByRole("button", { name: "Run query" }).click();
+  await expect(page.getByRole("status").nth(1)).toContainText("Query executed successfully");
+
+  const idbClientOutput2 = (await page.getByRole("code").textContent()) ?? "";
+  expect(JSON.parse(idbClientOutput2)).toEqual(result2);
 });
 
 test("create_WithOneToManyRelation_CreatesParentAndChildRecords", async () => {
