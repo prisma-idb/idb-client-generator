8060ae9642dffc21edafc349a7a19edfb43c942b
WhyAsh5114 <whyash5114@gmail.com>
2024-11-30 18:40:20 +0530

    test: add more tests for connect

--- packages/usage/tests/nestedQueries/connect.spec.ts
+++ packages/usage/tests/nestedQueries/connect.spec.ts
@@ -1,5 +1,81 @@
 import { test } from "../fixtures";
-import { expectQueryToSucceed } from "../queryRunnerHelper";
+import { expectQueryToFail, expectQueryToSucceed } from "../queryRunnerHelper";
+
+test("connect_ConnectProfileToUser_AddsForeignKeyToProfile", async ({ page }) => {
+  await expectQueryToSucceed({
+    page,
+    model: "user",
+    operation: "create",
+    query: { data: { name: "John" } },
+  });
+
+  await expectQueryToSucceed({
+    page,
+    model: "profile",
+    operation: "create",
+    query: { data: { bio: "John's bio", user: { connect: { id: 1 } } } },
+  });
+});
+
+test("connect_ConnectProfileToInvalidUser_ThrowsError", async ({ page }) => {
+  await expectQueryToFail({
+    page,
+    model: "profile",
+    operation: "create",
+    query: { data: { bio: "John's bio", user: { connect: { id: 1 } } } },
+    errorMessage: "Record not found",
+  });
+});
+
+test("connect_ConnectProfileToUserDirectly_AddsForeignKeyToProfile", async ({ page }) => {
+  await expectQueryToSucceed({
+    page,
+    model: "user",
+    operation: "create",
+    query: { data: { name: "John" } },
+  });
+
+  await expectQueryToSucceed({
+    page,
+    model: "profile",
+    operation: "create",
+    query: { data: { bio: "John's bio", userId: 1 } },
+  });
+});
+
+test("connect_ConnectProfileToInvalidUserDirectly_ThrowsError", async ({ page }) => {
+  await expectQueryToFail({
+    page,
+    model: "profile",
+    operation: "create",
+    query: { data: { bio: "John's bio", userId: 1 } },
+    errorMessage: "Record not found",
+  });
+});
+
+// TODO
+test.fixme("connect_ConnectTwoProfilesToUser_ThrowsError", async ({ page }) => {
+  await expectQueryToSucceed({
+    page,
+    model: "user",
+    operation: "create",
+    query: { data: { name: "John" } },
+  });
+
+  await expectQueryToSucceed({
+    page,
+    model: "profile",
+    operation: "create",
+    query: { data: { bio: "John's bio 1", user: { connect: { id: 1 } } } },
+  });
+
+  await expectQueryToSucceed({
+    page,
+    model: "profile",
+    operation: "create",
+    query: { data: { bio: "John's bio 2", user: { connect: { id: 1 } } } },
+  });
+});
 
 test("connect_ConnectValidPostsToUser_AddsForeignKeysToPosts", async ({ page }) => {
   await expectQueryToSucceed({
@@ -28,3 +104,17 @@ test("connect_ConnectValidPostsToUser_AddsForeignKeysToPosts", async ({ page }) 
     query: { include: { author: true } },
   });
 });
+
+test("connect_ConnectNonExistentProfile_ThrowsError", async ({ page }) => {
+  await expectQueryToFail({
+    page,
+    model: "user",
+    operation: "create",
+    query: {
+      data: { name: "John", profile: { connect: { id: 1 } } },
+    },
+    errorMessage: "",
+  });
+});
+
+// TODO: connect with conditions
