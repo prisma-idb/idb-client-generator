c9ad581218e7246255d4456f1fb3c1008fd58646
WhyAsh5114 <whyash5114@gmail.com>
2024-12-01 17:04:39 +0530

    test: tests for push and set scalar field updates

---
+++ packages/usage/tests/scalarListMethods/set.spec.ts
@@ -0,0 +1,46 @@
+import { test } from "../fixtures";
+import { expectQueryToSucceed } from "../queryRunnerHelper";
+
+test("set_SetNewTags_SuccessfullySetTags", async ({ page }) => {
+  await expectQueryToSucceed({
+    page,
+    model: "post",
+    operation: "create",
+    query: { data: { title: "Post1" } },
+  });
+
+  await expectQueryToSucceed({
+    page,
+    model: "post",
+    operation: "update",
+    query: { where: { id: 1 }, data: { tags: { set: ["Tag1", "Tag2"] } } },
+  });
+
+  await expectQueryToSucceed({
+    page,
+    model: "post",
+    operation: "findMany",
+  });
+});
+
+test("set_OverwriteExistingTags_SuccessfullyChangesTags", async ({ page }) => {
+  await expectQueryToSucceed({
+    page,
+    model: "post",
+    operation: "create",
+    query: { data: { title: "Post1", tags: ["Tag1", "Tag2"] } },
+  });
+
+  await expectQueryToSucceed({
+    page,
+    model: "post",
+    operation: "update",
+    query: { where: { id: 1 }, data: { tags: { set: ["Tag3", "Tag4"] } } },
+  });
+
+  await expectQueryToSucceed({
+    page,
+    model: "post",
+    operation: "findMany",
+  });
+});
