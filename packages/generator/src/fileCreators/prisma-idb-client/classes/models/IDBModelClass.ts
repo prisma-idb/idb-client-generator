c2f005938fc7ebffcedcdebf22b0c342922032da
WhyAsh5114 <whyash5114@gmail.com>
2024-12-10 18:17:39 +0530

    feat: added `updateMany()`

--- packages/generator/src/fileCreators/prisma-idb-client/classes/models/IDBModelClass.ts
+++ packages/generator/src/fileCreators/prisma-idb-client/classes/models/IDBModelClass.ts
@@ -4,27 +4,28 @@ import { addCountMethod } from "./api/count";
 import { addCreateMethod } from "./api/create";
 import { addCreateManyMethod } from "./api/createMany";
 import { addCreateManyAndReturn } from "./api/createManyAndReturn";
+import { addDeleteMethod } from "./api/delete";
+import { addDeleteManyMethod } from "./api/deleteMany";
 import { addFindFirstMethod } from "./api/findFirst";
 import { addFindFirstOrThrow } from "./api/findFirstOrThrow";
 import { addFindManyMethod } from "./api/findMany";
 import { addFindUniqueMethod } from "./api/findUnique";
 import { addFindUniqueOrThrow } from "./api/findUniqueOrThrow";
 import { addUpdateMethod } from "./api/update";
+import { addUpdateMany } from "./api/updateMany";
+import { addUpsertMethod } from "./api/upsert";
+import { addApplyOrderByClause } from "./utils/_applyOrderByClause";
 import { addApplyRelations } from "./utils/_applyRelations";
 import { addApplySelectClause } from "./utils/_applySelectClause";
 import { addApplyWhereClause } from "./utils/_applyWhereClause";
 import { addFillDefaultsFunction } from "./utils/_fillDefaults";
 import { addGetNeededStoresForCreate } from "./utils/_getNeededStoresForCreate";
 import { addGetNeededStoresForFind } from "./utils/_getNeededStoresForFind";
-import { addRemoveNestedCreateDataMethod } from "./utils/_removeNestedCreateData";
 import { addGetNeededStoresForWhere } from "./utils/_getNeededStoresForWhere";
-import { addDeleteMethod } from "./api/delete";
-import { addDeleteManyMethod } from "./api/deleteMany";
-import { addApplyOrderByClause } from "./utils/_applyOrderByClause";
+import { addPreprocessListFields } from "./utils/_preprocessListFields";
+import { addRemoveNestedCreateDataMethod } from "./utils/_removeNestedCreateData";
 import { addResolveOrderByKey } from "./utils/_resolveOrderByKey";
 import { addResolveSortOrder } from "./utils/_resolveSortOrder";
-import { addPreprocessListFields } from "./utils/_preprocessListFields";
-import { addUpsertMethod } from "./api/upsert";
 
 export function addIDBModelClass(file: SourceFile, model: Model, models: readonly Model[]) {
   const modelClass = file.addClass({
@@ -60,5 +61,6 @@ export function addIDBModelClass(file: SourceFile, model: Model, models: readonl
   addDeleteManyMethod(modelClass, model, models);
 
   addUpdateMethod(modelClass, model);
+  addUpdateMany(modelClass, model);
   addUpsertMethod(modelClass, model);
 }
