4b580e097c7e97fc5f57dfe56ff662869f1f4d5d
WhyAsh5114 <whyash5114@gmail.com>
2024-12-11 15:21:30 +0530

    refactor: move API tests into api folder

---
+++ packages/usage/tests/api/modelQueries/createMany.spec.ts
@@ -0,0 +1,17 @@
+import { test } from "../../fixtures";
+import { expectQueryToSucceed } from "../../queryRunnerHelper";
+
+test("createMany_ValidData_SuccessfullyCreatesRecords", async ({ page }) => {
+  await expectQueryToSucceed({
+    page,
+    model: "user",
+    operation: "createMany",
+    query: { data: [{ name: "John Doe" }, { name: "Alice" }] },
+  });
+
+  await expectQueryToSucceed({
+    page,
+    model: "user",
+    operation: "findMany",
+  });
+});
---
+++ packages/usage/tests/api/modelQueries/createManyAndReturn.spec.ts
@@ -0,0 +1,17 @@
+import { test } from "../../fixtures";
+import { expectQueryToSucceed } from "../../queryRunnerHelper";
+
+test("createManyAndReturn_SelectParticulars_SuccessfullyCreatesAndReturnsRecords", async ({ page }) => {
+  await expectQueryToSucceed({
+    page,
+    model: "user",
+    operation: "createManyAndReturn",
+    query: { data: [{ name: "John Doe" }, { name: "Alice" }], select: { id: true } },
+  });
+
+  await expectQueryToSucceed({
+    page,
+    model: "user",
+    operation: "findMany",
+  });
+});
