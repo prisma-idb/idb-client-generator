63a72012b4894ae497eee579cd065c4e42a778cc
WhyAsh5114 <whyash5114@gmail.com>
2025-01-08 17:37:02 +0530

    fix: DateTime update null handling

--- packages/generator/src/fileCreators/idb-utils/updateHandlers/DateTimeHandler.ts
+++ packages/generator/src/fileCreators/idb-utils/updateHandlers/DateTimeHandler.ts
@@ -38,10 +38,14 @@ export function addDateTimeUpdateHandler(utilsFile: SourceFile, models: readonly
         .conditionalWrite(nullableDateTimeFieldPresent, ` || dateTimeUpdate === null`)
         .writeLine(`)`)
         .block(() => {
-          writer.writeLine(`(record[fieldName] as ${fieldType}) = new Date(dateTimeUpdate);`);
+          writer.writeLine(
+            `(record[fieldName] as ${fieldType}) = dateTimeUpdate === null ? null : new Date(dateTimeUpdate);`,
+          );
         });
       writer.writeLine(`else if (dateTimeUpdate.set !== undefined)`).block(() => {
-        writer.writeLine(`(record[fieldName] as ${fieldType}) = new Date(dateTimeUpdate.set);`);
+        writer.writeLine(
+          `(record[fieldName] as ${fieldType}) = dateTimeUpdate.set === null ? null : new Date(dateTimeUpdate.set);`,
+        );
       });
     },
   });
