b2d4aefa92bdfd514fcb3be74c551d088d1aaceb
WhyAsh5114 <whyash5114@gmail.com>
2026-01-22 16:04:39 +0530

    ui: add shadcn-svelte components

--- packages/pidb-kanban-example/src/routes/+layout.svelte
+++ packages/pidb-kanban-example/src/routes/+layout.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import { ModeWatcher } from 'mode-watcher';
+	import { Toaster } from '$lib/components/ui/sonner';
 
 	let { children } = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
+
+<ModeWatcher />
+<Toaster />
+
 {@render children()}
