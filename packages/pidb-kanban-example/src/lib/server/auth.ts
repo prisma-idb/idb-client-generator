fe9b61061bf5030b85a32d14f3509fd58d78104a
WhyAsh5114 <whyash5114@gmail.com>
2026-01-23 19:38:18 +0530

    fix: better env management

--- packages/pidb-kanban-example/src/lib/server/auth.ts
+++ packages/pidb-kanban-example/src/lib/server/auth.ts
@@ -1,9 +1,9 @@
 import { betterAuth } from 'better-auth';
 import { prismaAdapter } from 'better-auth/adapters/prisma';
 import { prisma } from './prisma.js';
-import { BETTER_AUTH_SECRET, GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } from '$env/static/private';
 import { sveltekitCookies } from 'better-auth/svelte-kit';
 import { getRequestEvent } from '$app/server';
+import ENV from './env.js';
 
 export const auth = betterAuth({
 	database: prismaAdapter(prisma, {
@@ -11,10 +11,10 @@ export const auth = betterAuth({
 	}),
 	socialProviders: {
 		google: {
-			clientId: GOOGLE_CLIENT_ID,
-			clientSecret: GOOGLE_CLIENT_SECRET
+			clientId: ENV.GOOGLE_CLIENT_ID,
+			clientSecret: ENV.GOOGLE_CLIENT_SECRET
 		}
 	},
-	secret: BETTER_AUTH_SECRET,
+	secret: ENV.BETTER_AUTH_SECRET,
 	plugins: [sveltekitCookies(getRequestEvent)]
 });
--- packages/pidb-kanban-example/src/lib/server/prisma.ts
+++ packages/pidb-kanban-example/src/lib/server/prisma.ts
@@ -1,13 +1,13 @@
-import { DATABASE_URL } from '$env/static/private';
 import { PrismaClient } from '$lib/generated/prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
+import ENV from './env';
 
 const globalForPrisma = globalThis as typeof globalThis & { prisma?: PrismaClient };
 
 export const prisma =
 	globalForPrisma.prisma ??
 	new PrismaClient({
-		adapter: new PrismaPg({ connectionString: DATABASE_URL })
+		adapter: new PrismaPg({ connectionString: ENV.DATABASE_URL })
 	});
 
 if (process.env.NODE_ENV !== 'production') {
