89a028455fc8848b6a96c2255ea49356563ad0cc
WhyAsh5114 <whyash5114@gmail.com>
2026-01-29 21:32:15 +0530

    ui(docs): add theme and favicon

--- apps/docs/src/lib/layout.shared.tsx
+++ apps/docs/src/lib/layout.shared.tsx
@@ -1,9 +1,16 @@
 import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
+import Favicon from "./assets/favicon.png";
+import Image from "next/image";
 
 export function baseOptions(): BaseLayoutProps {
   return {
     nav: {
-      title: "Prisma IDB",
+      title: (
+        <>
+          <Image src={Favicon} alt="Prisma IDB Favicon" height={36} />
+          Prisma IDB
+        </>
+      ),
     },
   };
 }
