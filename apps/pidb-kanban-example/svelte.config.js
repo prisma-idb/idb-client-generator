e5ee12c3282be848321e03b6d15907367f2744ef
WhyAsh5114 <whyash5114@gmail.com>
2026-02-01 13:40:06 +0530

    fix: docs updates, and remove unused comments

--- apps/pidb-kanban-example/svelte.config.js
+++ apps/pidb-kanban-example/svelte.config.js
@@ -5,9 +5,6 @@ import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 const config = {
   preprocess: vitePreprocess(),
   kit: {
-    // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-    // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-    // See https://svelte.dev/docs/kit/adapters for more information about adapters.
     adapter: adapter(),
     serviceWorker: {
       register: false,
